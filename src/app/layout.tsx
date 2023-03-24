7ad480330eba0c912b88c4ddb72b966b3e5713ee
jaycdev2007 <jaycdev2007@gmail.com>
2023-03-23 17:29:52 -1200

    test adSense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,8 @@ export default function RootLayout({
     <html lang="pt">
       <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8601341061910635"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
+     <link rel="icon" type="image/x-icon" href="https://media.graphassets.com/output=format:jpg/00kwLz9HR6qzC4G7ojl9" />
       </head>
       <body className='flex flex-col justify-center items-center'>{children}</body>
     </html>
