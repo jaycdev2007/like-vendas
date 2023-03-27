1ea92bbedadd1868e144a6a1742a7f85e5bd9c50
jaycdev2007 <jaycdev2007@gmail.com>
2023-03-26 18:57:44 -1200

    resolver erros

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,19 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
+        <title>
+          LIkE VENDAS
+        </title>
+        <meta name='description' content='O Like Vendas é um site de controle de estoque
+            desenvolvido por Òscar Jeremias, um jovem angolano de 16 anos. 
+            A plataforma foi criada para ajudar empresas a gerenciar 
+            suas vendas e estoque de forma eficiente.
+            O site apresenta uma interface amigável e fácil de usar, com recursos que permitem
+            aos usuários registrar produtos, gerenciar entradas e saídas, monitorar níveis 
+            de estoque em tempo real e gerar relatórios de vendas e estoque.
+            Em resumo, o Like Vendas é um site de controle de estoque criado por
+            um jovem talentoso e empreendedor que tem como objetivo ajudar empresas a
+            gerenciar suas vendas e estoque de forma eficiente e eficaz.' />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8601341061910635"
      crossOrigin="anonymous"></script>
      <link rel="icon" type="image/x-icon" href="https://media.graphassets.com/output=format:jpg/00kwLz9HR6qzC4G7ojl9" />
--- src/controlles/actualizarServico.ts
+++ src/controlles/actualizarServico.ts
@@ -1,3 +1,4 @@
+
 export async function actualizarServico(id:string) {
     const date = new Date()
     const diasDeServico = 30
@@ -8,6 +9,7 @@ export async function actualizarServico(id:string) {
             mutation MyMutation {
                 updateLoja(data: {dataDeIncerramentoDoServico:"${date}"}, where: {id:"${id}"}){
                     id
+                    emailDaLoja
                   }
             }
         `
@@ -19,7 +21,7 @@ export async function actualizarServico(id:string) {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_ASSET_TOKEN}`,
         },
         body: form
-    })
+    }) 
 
     const data = await res.json()
 
